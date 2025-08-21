1121059434276792250a3c9ca57f5cbf49eb5352
Max-commits <tobamicar@gmail.com>
2025-08-21 21:11:30 +0100

    c

--- Scripts/fma.js
+++ Scripts/fma.js
@@ -169,7 +169,7 @@ class PerformanceOptimizer {
       const greatnessSection = document.querySelector(".greatness-section");
       if (greatnessSection) {
         greatnessSection.style.cssText += `
-          background-image: url('Images/IMG-20250804-WA0036.jpgc');
+          background-image: url('Images/IMG-20250804-WA0036.jpg');
           background-size: cover;
           background-position: center;
           background-repeat: no-repeat;
